89c6cf0e48bb976271c7fd85f0e1afea9214f48e
Paramanand Kumar <M1048697@mindtree.com>
2020-01-17 23:24:42 +0530

    second commit

--- orderingservice/src/main/java/com/eatza/order/OrderingserviceApplication.java
+++ orderingservice/src/main/java/com/eatza/order/OrderingserviceApplication.java
@@ -3,10 +3,12 @@ package com.eatza.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.eatza.order.config.JwtFilter;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderingserviceApplication {
 
